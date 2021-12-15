36aa889a320439b9b3cfcaec43367e791bc267e9
jonathan bola <bolajonathan@yahoo.com>
2021-12-14 18:04:55 -0600

    commit tree f180581f960e9a65069ee42ca073fdcf2c2d7802

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation){
+    if(pickupLocation>42){
+        return pickupLocation-42;
+    }
+    else{
+        return 42-pickupLocation;
+    }
+  }
+
+function distanceFromHqInFeet(someValue) {
+    ;
+    return distanceFromHqInBlocks(someValue)*264;
+      
+  }
+  function distanceTravelledInFeet(start, destination){
+      
+         if(start>destination){
+             return(start-destination)*264;
+         }
+         else{
+             return (destination-start)*264;
+         }
+  }
+function calculatesFarePrice(start, destination){
+    let fare = distanceTravelledInFeet(start, destination);
+    if(fare <=400){
+        return 0;
+    }
+    else if(fare>400 && fare <=2000){
+        return ((fare-400)*2)/100;
+    }
+    else if(fare>2000 && fare<2500){
+        return 25;
+    }
+    else{
+        return 'cannot travel that far';
+    }
+
+
+}
